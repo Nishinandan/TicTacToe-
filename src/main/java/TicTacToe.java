6654b09438104e6863be7154a66cb28840b68618
Nishinandan <hmnishi33@gmail.com>
2026-04-21 13:12:43 +0530

    UC5 - Move validation added

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,16 +1,50 @@
 public class TicTacToe {
 
+    static char[][] board = new char[3][3];
+
     public static void main(String[] args) {
-        int slot = 7;
-        System.out.println("Row: " + getRowFromSlot(slot));
-        System.out.println("Column: " + getColFromSlot(slot));
+        initializeBoard();
+
+        int slot = 5; // test input
+
+        int row = (slot - 1) / 3;
+        int col = (slot - 1) % 3;
+
+        if (isValidMove(row, col)) {
+            System.out.println("Valid Move");
+        } else {
+            System.out.println("Invalid Move");
+        }
+
+        printBoard();
     }
 
-    static int getRowFromSlot(int slot) {
-        return (slot - 1) / 3;
+    static void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
     }
 
-    static int getColFromSlot(int slot) {
-        return (slot - 1) % 3;
+    static boolean isValidMove(int row, int col) {
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+
+        if (board[row][col] != '-') {
+            return false;
+        }
+
+        return true;
+    }
+
+    static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
